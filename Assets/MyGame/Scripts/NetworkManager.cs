2e01985b524a40828fc51b873ebbe066fb392a9c
Lucas Pigaiani <lucaspigaiani@gmail.com>
2025-01-31 10:11:43 -0300

    Project init and imported PUN2

---
+++ Assets/MyGame/Scripts/NetworkManager.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class NetworkManager : MonoBehaviourPunCallbacks
+{
+    void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnected()
+    {
+        Debug.Log("OnConnected");
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("OnConnectedToMaster");
+
+        PhotonNetwork.JoinRandomRoom();
+    }
+
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        Debug.Log("OnConnectedToMaster");
+
+        RoomOptions roomOptions = new();
+        roomOptions.MaxPlayers = 4;
+
+        PhotonNetwork.CreateRoom(null, roomOptions);
+    }
+
+    public override void OnJoinedRoom()
+    {
+        Debug.Log("OnJoinedRoom");
+    }
+}
